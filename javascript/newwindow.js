96894ce0e7718e403cf0fe69fe6e0b60e486e057
rene (SVN Import) <info@achievo.org>
2001-09-06 10:10:35 +0000

    fixed spelfout in scrollbarS

--- javascript/newwindow.js
+++ javascript/newwindow.js
@@ -1,7 +1,7 @@
 function NewWindow(mypage, myname, w, h, scroll, resize) {
   var winl =  10;
   var wint =  10;
-  winprops = "height="+h+",width="+w+",top="+wint+",left="+winl+",scrollbar="+scroll+","+resize
+  winprops = "height="+h+",width="+w+",top="+wint+",left="+winl+",scrollbars="+scroll+","+resize
   win = window.open(mypage, myname, winprops)
   if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
 }
