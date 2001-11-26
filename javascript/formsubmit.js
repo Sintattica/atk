5ffdbf5e9a8ecf2bb8ac5ac2503bde2eb47a8045
ivo (SVN Import) <info@achievo.org>
2001-11-26 14:24:33 +0000

    Added globalSubmit call to formsubmit.

--- javascript/formsubmit.js
+++ javascript/formsubmit.js
@@ -2,5 +2,9 @@
 function atkSubmit(target)
 {
   document.entryform.atkescape.value = target;
+  
+  // call global submit function, which doesn't get called automatically
+  // when we call entryform.submit manually.
+  globalSubmit(document.entryform);
   document.entryform.submit();
 }
