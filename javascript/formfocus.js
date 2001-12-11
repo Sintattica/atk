97270e83b4febb99d012d42da78bc2c3f475b63f
sandy (SVN Import) <info@achievo.org>
2001-12-11 09:00:09 +0000

    some bugfixes, new var in layout file for rowmouseover, some bugfixes, and maybe some new bugs :)

--- javascript/formfocus.js
+++ javascript/formfocus.js
@@ -1,11 +1,15 @@
-function placeFocus() {
-  if (document.forms.length > 0) {
+function placeFocus()
+{
+  alert('PLACEFOCUS');
+  if (document.forms.length > 0) 
+  {
     var field = document.forms[0];
-    for (i = 0; i < field.length; i++) {
-      if ((field.elements[i].type == "text") || (field.elements[i].type ==
-        "textarea") || (field.elements[i].type.toString().charAt(0) == "s")) {
-          document.forms[0].elements[i].focus();
-          break;
+    for (i = 0; i < field.length; i++) 
+    {
+      if ((field.elements[i].type == "text") || (field.elements[i].type =="textarea") || (field.elements[i].type.toString().charAt(0) == "s")) 
+      {
+        document.forms[0].elements[i].focus();
+        break;
       }
     }
   }
