529d68a4d134e862aa89bbbc97f6ee13d7e7a1bf
ivo (SVN Import) <info@achievo.org>
2002-02-20 16:14:04 +0000

    Made atknodetype variable more safe, by passing it in all urls and forms instead of relying on the session. Also made a userfriendly screen if session is lost.

--- skel/dispatch.php
+++ skel/dispatch.php
@@ -3,20 +3,30 @@
   
   include_once("atk.inc");
   
-  atksession();
+  atksession("beheer");
   atksecure();
-   
-  // Create node
-  $obj = getNode($ATK_VARS["atknodetype"]); 
-
-  if (is_object($obj))
+  
+  if($ATK_VARS["atknodetype"]=="")
   {
-    $obj->dispatch($ATK_VARS);     
+    // no nodetype passed, or session expired
+    $g_layout->ui_top(text("title_session_expired"));
+    $g_layout->output("<br><br>".text("explain_session_expired")."<br><br><br><br>");
+    $g_layout->ui_bottom(); 
+    $g_layout->page(text("title_session_expired"));
   }
   else
-  {
-    atkdebug("No object created!!?!");
-  }
+  {       
+    // Create node
+    $obj = getNode($ATK_VARS["atknodetype"]); 
 
+    if (is_object($obj))
+    {
+      $obj->dispatch($ATK_VARS);     
+    }
+    else
+    {
+      atkdebug("No object created!!?!");
+    }
+  }
   $g_layout->outputFlush();
 ?>
