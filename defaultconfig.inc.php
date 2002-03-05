45601458615482e279ad9efee7a65e28d0ac152a
peter (SVN Import) <info@achievo.org>
2002-03-05 15:11:07 +0000

    Added a new configuration directive ($config_tabs) which can be used to enable/disable the use of tabs (defaults to enabled).

--- defaultconfig.inc.php
+++ defaultconfig.inc.php
@@ -10,12 +10,12 @@
   $config_database="mysql";
 
   $config_languagefile="english.lng";
-  $config_recordsperpage=25; 
+  $config_recordsperpage=25;
 
   // security
   $config_authentication = "none";
   $config_authentication_md5 = true;
-  $config_authentication_cookie = false; 
+  $config_authentication_cookie = false;
   $config_authentication_cookie_expire = 10080; // the default cookie expiry time (in minutes) (7 days)
   $config_authentication_session = true;
   $config_securityscheme = "none";
@@ -26,44 +26,46 @@
   $config_auth_leveltable  = ""; // defaults to usertable
   $config_auth_accesstable = "access";
   $config_auth_userfield   = "userid";
-  $config_auth_passwordfield = "password";  
+  $config_auth_passwordfield = "password";
   $config_auth_levelfield = "entity";
   $config_auth_mail_port = "110"; // default pop3 port
   $config_auth_mail_virtual = false; // no vmail.
-  
+
   $config_logging = 0; // no logging;
   $config_logfile = "/tmp/atk-security.log";
 
   $config_debug = 0;
   $config_halt_on_error = "critical";
-  
+
   // Layout config
   $config_menu_delimiter = "<br>";
   $config_menu_pos = "left";
   $config_menu_layout = "default";
   $config_top_frame = 0; // 0 = no   - 1 = yes
   $config_defaulttheme = "default";
- 
+
   // Display a 'stack' of the user activities in the top right corner.
   $config_stacktrace = true;
 
   // An administrator password that is empty will *DISABLE* administrator login!
   $config_administratorpassword = "";
-  
+
   // A guest password that is empty will *DISABLE* guest login!
   $config_guestpassword = "";
 
   // Module path (without trailing slash!)
   $config_module_path = $config_atkroot."modules";
-  
+
   // Automatic error reporting is turned off by default.
   $config_mailreport = "";
 
   $config_search_defaultmode = "substring";
-  
-  // Whether the action links in a recordlist appear left or right 
+
+  // Whether the action links in a recordlist appear left or right
   $config_recordlist_orientation = "right";
 
   $config_enable_ie_extensions = false;
 
+  // Whatever tabs are enabled or not
+  $config_tabs = true;
 ?>
