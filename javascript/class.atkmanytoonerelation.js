d875bf236783611d6b95a9e03b116ba4b32c882d
ivo (SVN Import) <info@achievo.org>
2002-05-05 07:03:28 +0000

    Added javascript file belonging to AF_RELATION_AUTOLINK.

---
+++ javascript/class.atkmanytoonerelation.js
@@ -0,0 +1,6 @@
+
+  function mto_parse(link, value)
+  {
+     var atkselector = value.replace("='", "_1253D_12527").replace("'", "_12527");
+     return link.replace('REPLACEME', atkselector);
+  }
