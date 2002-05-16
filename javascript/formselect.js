b43ab5800bb04df90a06fd26378d58884ebcd553
peter (SVN Import) <info@achievo.org>
2002-05-16 07:24:04 +0000

    Introduction of the new multi-record action feature. When you enable the multi-record actions with the special NF_MULTI_RECORD_ACTIONS flag for atkNode, it's possible to "execute" an action for multiple records at once. The default multi-record action is "delete", but you can add or remove actions with the third (optional) argument for the recordActions(...) method. You will get an array of actions, e.g. array("delete", ...) in which you can simply add actions or optinally remove actions using the unset PHP function.
    Please note that this feature is disabled by default! It's only enabled when
    you use the special NF_MULTI_RECORD_ACTIONS flag. At the moment only the ATK
    delete action has support for handling multi-record actions, in the near
    future this set may be extended. If you build your own action method, the only
    difference between a single record action and a multi-record action is that
    for the latter the "atkselector" variable will contain an array of selectors!
    
    WARNING: this feature is experimental, and might contain bugs. However, if
    you don't use the feature, you probably won't encounter any problems. Ofcourse
    the feature is well-tested, so you probably won't run into problems when using
    it. This means you won't get ice for any found bug, unless it affects ATK
    applications which don't use the new flag. :)

---
+++ javascript/formselect.js
@@ -0,0 +1,98 @@
+/**
+ * Updates the selection of select boxes for the record list form.
+ * @param name unique recordlist name
+ * @param type "all", "none" or "invert"
+ */
+function updateSelection(name, type)
+{
+  /* get selectors */
+  var list = document.entryform.elements[name + '_atkselector[]'];
+
+  /* no selectors?! impossible situation, bail out! */
+  if (typeof(list) == 'undefined') return;
+
+  /* walk through list */
+  if (typeof(list.length) == 'undefined') list = new Array(list);
+  for (var i = 0; i < list.length; i++)
+  {
+			if      ("all"    == type && !list[i].disabled)	list[i].checked = true;
+      else if ("none"   == type && !list[i].disabled)	list[i].checked = false;
+      else if ("invert" == type && !list[i].disabled)	list[i].checked = !list[i].checked;
+  }
+}
+
+/**
+ * Disables / enables checkboxes depending if the record supports
+ * a certain action or not.
+ * @param name unique recordlist name
+ */
+function updateSelectable(name)
+{
+  /* get selectors */
+  var list = document.entryform.elements[name + '_atkselector[]'];
+
+  /* no selectors?! impossible situation, bail out! */
+  if (typeof(list) == 'undefined') return;
+
+  /* some stuff we need to know */
+  var index  = document.entryform.elements[name + '_atkaction'].selectedIndex;
+  var action = document.entryform.elements[name + '_atkaction'][index].value;
+
+  /* walk through list */
+  if (typeof(list.length) == 'undefined') list = new Array(list);
+  for (var i = 0; i < list.length; i++)
+  {
+    /* supported actions */
+    var actions = eval(name + '["' + list[i].value + '"]');
+    if (typeof(actions) == 'undefined') actions = new Array();
+
+    /* contains action? */
+    disabled = true;
+    for (var j = 0; disabled && j < actions.length; j++)
+      if (actions[j] == action) disabled = false
+
+    /* disable */
+    list[i].disabled = disabled;
+    if (disabled) list[i].checked = false;
+  }
+}
+
+/**
+ * Because we allow embedded recordLists for 1:n relations we need a way to somehow
+ * distinguish between the submit of the edit form, and the submit of the multi-record action.
+ * This method uses the atkescape option to redirect the multi-record action to a level higher
+ * on the session stack, which makes it possible to return to the edit form (saving updated values!)
+ * @param name unique recordlist name
+ * @param target where do we escape to?
+ */
+function atkSubmitMRA(name, target)
+{
+  /* initial target URL */
+  atknodetype = document.entryform.elements[name + '_atknodetype'].value;
+  atkaction   = document.entryform.elements[name + '_atkaction'].value;
+  target += 'atknodetype=' + atknodetype + '&atkaction=' + atkaction;
+
+  /* get selectors */
+  var list = document.entryform.elements[name + '_atkselector[]'];
+
+  /* no selectors?! impossible situation, bail out! */
+  if (typeof(list) == 'undefined') return;
+
+  /* add the selectors to the target URL */
+  var selectorLength = 0;
+  if (typeof(list.length) == 'undefined') list = new Array(list);
+  for (var i = 0; i < list.length; i++)
+    if (!list[i].disabled && list[i].checked)
+    {
+      target += '&atkselector[]=' + list[i].value;
+      selectorLength++;
+    }
+
+  /* change atkescape value and submit form */
+  if (selectorLength > 0)
+  {
+    document.entryform.atkescape.value = target;
+    globalSubmit(document.entryform);
+    document.entryform.submit();
+  }
+}
