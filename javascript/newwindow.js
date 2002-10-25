31fc156b4febe62843099f1791ec25ef679d1b5e
maurice (SVN Import) <info@achievo.org>
2002-10-25 12:07:06 +0000

    Added parameters statusbar, titlebar, personalbar, menubar and toolbar. Default value is 'no'

--- javascript/newwindow.js
+++ javascript/newwindow.js
@@ -1,8 +1,15 @@
  
-function NewWindow(mypage, myname, w, h, scroll, resize) {
+function NewWindow(mypage, myname, w, h, scroll, resize, statusbar, menubar, toolbar, personalbar, titlebar) {
   var winl =  10;
   var wint =  10;
-  winprops = "height="+h+",width="+w+",top="+wint+",left="+winl+",scrollbars="+scroll+","+resize
+  scroll = scroll || 'no';
+  statusbar = statusbar || 'no';
+  menubar=menubar || 'no';
+  toolbar= toolbar || 'no';
+  personalbar= personalbar || 'no';
+  titlebar= titlebar || 'no';
+  resizable= resize || 'no';
+  winprops = "height="+h+",width="+w+",top="+wint+",left="+winl+",scrollbars="+scroll+", resizable="+resizable+",status="+statusbar+",menubar="+menubar+",toolbar="+toolbar+",personalbar="+personalbar+",titlebar="+titlebar;
   win = window.open(mypage, myname, winprops)
   if (parseInt(navigator.appVersion) >= 4) { win.window.focus(); }
 }
