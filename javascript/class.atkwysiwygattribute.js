7780dc04c9f7eb990cee2c7004668bcc92b0e2c2
lineke (SVN Import) <info@achievo.org>
2003-06-02 14:54:21 +0000

    Added javascript for internal links support

--- javascript/class.atkwysiwygattribute.js
+++ javascript/class.atkwysiwygattribute.js
@@ -129,3 +129,36 @@
      }
    }
  }
+
+ function modifySelection(pre, post)
+{
+  if (!document.selection) return;
+
+  var selection = obj_editor.document.selection.createRange();
+  var current   = selection.text;
+
+  if (current == '') return;
+
+  selection.text = pre + current + post;
+  selection.parentElement().focus();
+}
+
+function popupSelection(url,title)
+{
+  if (!document.selection)
+  {
+   alert("Selecteer eerst het stuk tekst waar u een link van wilt maken");
+   return;
+  }
+
+  var selection = obj_editor.document.selection.createRange();
+  var current   = selection.text;
+
+  if (current == '')
+  {
+    alert("Selecteer eerst het stuk tekst waar u een link van wilt maken");
+    return;
+  }
+
+  NewWindow(url,title,400,400,'yes');
+}
