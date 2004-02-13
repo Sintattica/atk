138edf7915640b0d5b68e82515be9273a68d349d
ivo (SVN Import) <info@achievo.org>
2004-02-13 07:54:46 +0000

    Added onChange javascript support to atklistattribute, and an autohide method that hides attributes based on listbox value.

---
+++ javascript/dhtml_formtools.js
@@ -0,0 +1,11 @@
+function hideAttrib(attrib)
+{
+  el = document.getElementById('ar_'+attrib);
+  el.style.display="none";
+}
+
+function showAttrib(attrib)
+{
+  el = document.getElementById('ar_'+attrib);
+  el.style.display='';
+}
