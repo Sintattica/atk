9347853e648ee981764e7a1a2e9cadd959f3f40e
ivo (SVN Import) <info@achievo.org>
2004-10-11 22:02:12 +0000

    Added Slovak translation (contributed by Daniel Kulifaj)

---
+++ javascript/calendar/lang/calendar-sk.js
@@ -0,0 +1,45 @@
+// ** I18N
+Calendar._DN = new Array
+("Nede¾a",
+ "Pondelok",
+ "Utorok",
+ "Streda",
+ "Štvrtok",
+ "Piatok",
+ "Sobota",
+ "Nede¾a");
+Calendar._MN = new Array
+("Január",
+ "Február",
+ "Marec",
+ "Apríl",
+ "Máj",
+ "Jún",
+ "Júl",
+ "August",
+ "September",
+ "Október",
+ "November",
+ "December");
+
+// tooltips
+Calendar._TT = {};
+Calendar._TT["TOGGLE"] = "Zmena prvého dòa v tıdni";
+Calendar._TT["PREV_YEAR"] = "Predch. rok (pridr pre menu)";
+Calendar._TT["PREV_MONTH"] = "Predch. mesiac (pridr pre menu)";
+Calendar._TT["GO_TODAY"] = "Dnešnı dátum";
+Calendar._TT["NEXT_MONTH"] = "Ïalší mesiac (pridr pre menu)";
+Calendar._TT["NEXT_YEAR"] = "Ïalší rok (pridr pre menu)";
+Calendar._TT["SEL_DATE"] = "Vyber dátum";
+Calendar._TT["DRAG_TO_MOVE"] = "Chy a presuò";
+Calendar._TT["PART_TODAY"] = " (dnes)";
+Calendar._TT["MON_FIRST"] = "Zobraz ako prvı Pondelok";
+Calendar._TT["SUN_FIRST"] = "Zobraz ako prvú Nede¾u";
+Calendar._TT["CLOSE"] = "Zatvor";
+Calendar._TT["TODAY"] = "Dnes";
+
+// date formats
+Calendar._TT["DEF_DATE_FORMAT"] = "d.mm.yy";
+Calendar._TT["TT_DATE_FORMAT"] = "D, M y";
+
+Calendar._TT["WK"] = "tı";
