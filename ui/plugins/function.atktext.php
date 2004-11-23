77deb3d3dbcceb6f8f9ec9373e5e605182e43234
ivo (SVN Import) <info@achievo.org>
2004-11-23 22:15:15 +0000

    Added {atktext} tag for use in templates. (wrapper for atktext() call)

---
+++ ui/plugins/function.atktext.php
@@ -0,0 +1,33 @@
+<?php
+ /*
+  * function to get multilanguage strings
+  *
+  * This is actually a wrapper for ATK's atktext() method, for
+  * use in templates.
+  *
+  * @author Ivo Jansch <ivo@achievo.org>
+  *
+  * Example: {atktext id="users.userinfo.description"}
+  *          {atktext id="userinfo.description" module="users"}
+  *          {atktext id="description" module="users" node="userinfo"}
+  *
+  */
+ function smarty_function_atktext($params, &$smarty)
+ {
+   switch(substr_count($params["id"], "."))
+   {     
+     case 1: 
+     {
+       list($node, $id) = explode(".", $params["id"]);
+       return atktext($id, $params["module"], $node);
+     }
+     case 2:
+     {
+       list($module, $node, $id) = explode(".", $params["id"]);       
+       return atktext($id, $module, $node);
+     }
+     default: return atktext($params["id"], $params["module"], $params["node"]);
+   }     
+ }
+
+?>
