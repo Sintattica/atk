06721f45ed461daf6519405a9c54275d025cc715
ivo (SVN Import) <info@achievo.org>
2005-03-24 00:09:20 +0000

    Added test.php wrapper for performing unit tests.

---
+++ skel/test.php
@@ -0,0 +1,30 @@
+<?php
+  /**
+   * This file is part of the Achievo ATK distribution.
+   * Detailed copyright and licensing information can be found
+   * in the doc/COPYRIGHT and doc/LICENSE files which should be
+   * included in the distribution.
+   *
+   * @package atk
+   * @subpackage skel
+   *
+   * @copyright (c)2005 Ivo Jansch   
+   * @license http://www.achievo.org/atk/licensing ATK Open Source License
+   *
+   * @version $Revision$
+   * $Id$
+   */  
+
+  /**
+   * @internal includes 
+   */
+  $config_atkroot = "./";
+  include_once("atk.inc");
+  
+  atksession();
+  atksecure();
+  
+  $suite = &atknew("atk.test.atktestsuite");  
+  $suite->run(); 
+
+?>
