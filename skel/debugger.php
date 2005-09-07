e820defa1cab564d2d507754e6fbab3206664e16
ivo (SVN Import) <info@achievo.org>
2005-09-07 23:33:23 +0000

    + Added ATK debug console for debugging queries. Console can be opened   from the debugoutput. It can lists all queries performed on a page, but   also zoom in on one query and give the following details:   * Resultset; partial set of the results. user can click to see full     results. Useful to debug raw query output.   * Explain plan; The mysql explain plan for the query, to debug     performance   * Backtrace; shows the call history that leads up to execution of the     query.
    This feature is currently implemented for MySQL only.

---
+++ skel/debugger.php
@@ -0,0 +1,45 @@
+<?php
+
+  /**
+   * This file is part of the Achievo ATK distribution.
+   * Detailed copyright and licensing information can be found
+   * in the doc/COPYRIGHT and doc/LICENSE files which should be 
+   * included in the distribution.
+   *
+   * This file is the skeleton dispatcher file, which you can copy
+   * to your application dir and modify if necessary. By default, it
+   * checks the $atknodetype and $atkaction postvars and creates the
+   * node and dispatches the action.
+   *
+   * @package atk
+   * @subpackage skel
+   *
+   * @author Ivo Jansch <ivo@achievo.org>
+   *
+   * @copyright (c)2000-2004 Ivo Jansch
+   * @license http://www.achievo.org/atk/licensing ATK Open Source License
+   *
+   * @version $Revision$
+   * $Id$
+   */
+   
+  /**
+   * @internal Setup the system
+   */
+  $config_atkroot = "./";
+
+  include_once($config_atkroot."atk/include/initial.inc");
+
+  atksession();
+  atksecure();
+
+  atkimport("atk.utils.atkdebugger");
+  $debugger = &atkDebugger::getInstance(true);    
+
+  $output = &atkOutput::getInstance();
+
+  $output->output($debugger->renderConsole());
+
+  $config_debug = 0; // force debugging off in console
+  $output->outputFlush();
+?>
