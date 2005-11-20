1bc86c050ebfc30885df45119d2d431badc2d05d
ivo (SVN Import) <info@achievo.org>
2005-11-20 16:38:36 +0000

    + Added Polish language file for atkDateAttribute. (bug #560) [Przemek]

---
+++ javascript/calendar/lang/calendar-pl.js
@@ -0,0 +1,45 @@
+// ** I18N
+Calendar._DN = new Array
+("Niedziela",
+ "Poniedzia³ek",
+ "Wtorejk",
+ "¦roda",
+ "Czwartek",
+ "Pi±tek",
+ "Sobota",
+ "Niedziela");
+Calendar._MN = new Array
+("Styczeñ",
+ "Luty",
+ "Marzec",
+ "Kwiecieñ",
+ "Maj",
+ "Czerwiec",
+ "Lipiec",
+ "Sierpieñ",
+ "Wrzesieñ",
+ "Pa¼dziernik",
+ "Listopad",
+ "Grudzieñ");
+
+// tooltips
+Calendar._TT = {};
+Calendar._TT["TOGGLE"] = "Prze³±cz pierwszy dzieñ tygodnia";
+Calendar._TT["PREV_YEAR"] = "Poprz. rok (przytrzymaj -> menu)";
+Calendar._TT["PREV_MONTH"] = "Poprz. miesi±c (przytrzymaj -> menu)";
+Calendar._TT["GO_TODAY"] = "Przejd¼ do dzisiejszej daty";
+Calendar._TT["NEXT_MONTH"] = "Nast. miesi±c (przytrzymaj -> menu)";
+Calendar._TT["NEXT_YEAR"] = "Nast. rok (przytrzymaj -> menu)";
+Calendar._TT["SEL_DATE"] = "Wybierz datê";
+Calendar._TT["DRAG_TO_MOVE"] = "Przeci±gnij";
+Calendar._TT["PART_TODAY"] = " (dzi¶)";
+Calendar._TT["MON_FIRST"] = "Poniedzia³ek jako pierwszy";
+Calendar._TT["SUN_FIRST"] = "Niedziela jako pierwszy";
+Calendar._TT["CLOSE"] = "Zamknij";
+Calendar._TT["TODAY"] = "Dzi¶";
+
+// date formats
+Calendar._TT["DEF_DATE_FORMAT"] = "y-mm-dd";
+Calendar._TT["TT_DATE_FORMAT"] = "DD, d MM y";
+
+Calendar._TT["WK"] = "tyg.";
