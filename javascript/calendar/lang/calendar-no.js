5e9686d70dcd0742a1179b1cc47537704ad13cf0
ivo (SVN Import) <info@achievo.org>
2005-11-30 20:55:25 +0000

    * Updated Norwegian (no) translation (contributed by Olav B. Lygre) * Updated Brazilian Portuguese (bp) translation  (contributed by   Elton Luis Minetto) * Regenerated all languagefiles for upcoming 5.3 release

---
+++ javascript/calendar/lang/calendar-no.js
@@ -0,0 +1,45 @@
+// ** I18N
+Calendar._DN = new Array
+("Søndag",
+ "Mandag",
+ "Tirsdag",
+ "Onsdag",
+ "Torsdag",
+ "Fredag",
+ "Lørdag",
+ "Søndag");
+Calendar._MN = new Array
+("Januar",
+ "Februar",
+ "Mars",
+ "April",
+ "Mai",
+ "Juni",
+ "Juli",
+ "August",
+ "September",
+ "Oktober",
+ "November",
+ "Desember");
+
+// tooltips
+Calendar._TT = {};
+Calendar._TT["TOGGLE"] = "Endre første dag i uken";
+Calendar._TT["PREV_YEAR"] = "Forrige år (hold for meny)";
+Calendar._TT["PREV_MONTH"] = "Forrige måned (hold for meny)";
+Calendar._TT["GO_TODAY"] = "Gå til idag";
+Calendar._TT["NEXT_MONTH"] = "Neste måned (hold for meny)";
+Calendar._TT["NEXT_YEAR"] = "Neste år (hold for meny)";
+Calendar._TT["SEL_DATE"] = "Velg dato";
+Calendar._TT["DRAG_TO_MOVE"] = "Dra for å flytte";
+Calendar._TT["PART_TODAY"] = " (idag)";
+Calendar._TT["MON_FIRST"] = "Vis mandag først";
+Calendar._TT["SUN_FIRST"] = "Vis søndag først";
+Calendar._TT["CLOSE"] = "Lukk";
+Calendar._TT["TODAY"] = "Idag";
+
+// date formats
+Calendar._TT["DEF_DATE_FORMAT"] = "y-mm-dd";
+Calendar._TT["TT_DATE_FORMAT"] = "D, M d";
+
+Calendar._TT["WK"] = "Uke";
