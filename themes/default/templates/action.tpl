cc55ddd710ac1545c4a5c4d8b33f341f84820cc9
patrick (SVN Import) <info@achievo.org>
2006-03-20 16:16:33 +0000

    Added atkMessageQueue and atkmessages plugin for user notification messages.

--- themes/default/templates/action.tpl
+++ themes/default/templates/action.tpl
@@ -1,13 +1,22 @@
 {$formstart}
 <table border="0" cellspacing="0" cellpadding="2" width="100%">
   {if $helplink!=""}<tr><td align="right" class="helplink">{$helplink}</td></tr>{/if}
+    <td align="center" valign="top">
+      <br>
+      <div class="atkmessages">
+        {atkmessages}
+        {foreach from=$atkmessages item=message}
+          &nbsp;{$message}<br>
+        {/foreach}
+      </div>
+    </td>
   <tr>
     <td valign="top" align="center">{$content}<br></td>
   </tr>
   <tr>
     <td align="center" valign="top">
       {foreach from=$buttons item=button}
-        &nbsp;{$button}&nbsp;        
+        &nbsp;{$button}&nbsp;
       {/foreach}<br><br></td>
   </tr>
 </table>
---
+++ ui/plugins/function.atkmessages.php
@@ -0,0 +1,53 @@
+<?php
+
+  /**
+   * This file is part of the Achievo ATK distribution.
+   * Detailed copyright and licensing information can be found
+   * in the doc/COPYRIGHT and doc/LICENSE files which should be
+   * included in the distribution.
+   *
+   * @package atk
+   * @subpackage ui
+   *
+   * @copyright (c)2000-2004 Ivo Jansch
+   * @license http://www.achievo.org/atk/licensing ATK Open Source License
+   *
+   * @version $Revision$
+   * $Id$
+   */
+
+  atkimport("atk.utils.atkmessagequeue");
+
+  /**
+   * Implements the {atkmessages} plugin for use in templates.
+   *
+   * The {atkmessages} tag does not output anything. Instead, it loads
+   * the messages into the template variable {$atkmessages}, which is
+   * an array of elements, each with a single message.
+   *
+   * <b>Example:</b>
+   * <code>
+   *   {atkmessages}
+   *
+   *   {foreach from=$atkmessages item=message}
+   *     {$message}<br>
+   *   {/foreach}
+   * </code>
+   *
+   * @author Patrick van der Velden <patrick@ibuildings.nl>
+   *
+   */
+  function smarty_function_atkmessages($params, &$smarty)
+  {
+    global $g_sessionManager;
+    if (is_object($g_sessionManager))
+    {
+      $msgs =  atkMessageQueue::getMessages();
+      $smarty->assign("atkmessages", $msgs);
+      atk_var_dump($msgs, "Messages");
+      return "";
+    }
+    return "";
+  }
+
+?>
