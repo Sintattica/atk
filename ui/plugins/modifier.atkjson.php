553e8846ff805f5c12935cd8153779417a814955
peter (SVN Import) <info@achievo.org>
2006-06-23 15:18:42 +0000

    + Added JSON serialization/deserialization support, available as static   methods of class atk.utils.atkJSON and available as Smarty modifier. [Peter]

---
+++ ui/plugins/modifier.atkjson.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of the Achievo ATK distribution.
+ * Detailed copyright and licensing information can be found
+ * in the doc/COPYRIGHT and doc/LICENSE files which should be
+ * included in the distribution.
+ *
+ * @package atk
+ * @subpackage ui
+ *
+ * @copyright (c) 2004 Ivo Jansch
+ * @license http://www.achievo.org/atk/licensing ATK Open Source License
+ *
+ * @version $Revision$
+ * $Id$
+ */
+
+/**
+ * Modifier to encode a Smarty/PHP variable to JSON.
+ * 
+ * Example of usage:
+ * {$var|@atkjson}
+ *
+ * @author Peter C. Verhage <peter@ibuildings.nl>
+ */
+function smarty_modifier_atkjson($data)
+{
+  atkimport('atk.utils.atkjson');
+  atk_var_dump(atkJSON::encode($data), "BLUB");
+  return atkJSON::encode($data);
+}
+?>
