ac4e25fbddf40e81f3c16c55397a1a6d802e0f41
lineke (SVN Import) <info@achievo.org>
2007-02-02 09:35:48 +0000

    + Added winxp theme. [Lineke] + Added atkconfig plugin for setting a smarty var with config variable.   [Lineke]

---
+++ ui/plugins/function.atkconfig.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Achievo ATK distribution.
+ * Detailed copyright and licensing information can be found
+ * in the doc/COPYRIGHT and doc/LICENSE files which should be
+ * included in the distribution.
+ *
+ * @package atk
+ * @subpackage ui
+ *
+ * @copyright (c)2004 Ivo Jansch
+ * @license http://www.achievo.org/atk/licensing ATK Open Source License
+ *
+ * @version $Revision$
+ * $Id$
+ */
+
+/**
+ * Function for getting a configuration variable and set this in a variable in smarty.
+ * 
+ * @author Lineke Kerckhoffs-Willems <lineke@ibuildings.nl>
+ */
+function smarty_function_atkconfig($params, &$smarty)
+{
+  $smarty->assign($params["smartyvar"], atkconfig($params["var"]));
+}
+
+?>
