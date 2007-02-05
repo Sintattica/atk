1934a7606bf89da5951df059b21af4c31cb73216
guido (SVN Import) <info@achievo.org>
2007-02-05 21:53:38 +0000

    * Updated atk skel files with achievo changes. [Guido]

--- skel/dispatch.php
+++ skel/dispatch.php
@@ -45,16 +45,17 @@
 
     $page->register_style($theme->stylePath("style.css"));
 
-    $destination = "";
+    $destination = "index.php?atklogout=true";
     if(isset($ATK_VARS["atknodetype"]) && isset($ATK_VARS["atkaction"]))
     {
-      $destination = "&atknodetype=".$ATK_VARS["atknodetype"]."&atkaction=".$ATK_VARS["atkaction"];
+      $destination .= "&atknodetype=".$ATK_VARS["atknodetype"]."&atkaction=".$ATK_VARS["atkaction"];
       if (isset($ATK_VARS["atkselector"])) $destination.="&atkselector=".$ATK_VARS["atkselector"];
     }
 
-    $box = $ui->renderBox(array("title"=>atktext("title_session_expired"),
-                                "content"=>'<br><br>'.atktext("explain_session_expired").'<br><br><br><br>
-                                           <a href="index.php?atklogout=true'.$destination.'" target="_top">'.atktext("relogin").'</a><br><br>'));
+    $title = atktext("title_session_expired");
+    $contenttpl = '<br>%s<br><br><input type="button" onclick="top.location=\'%s\';" value="%s"><br><br>';
+    $content = sprintf($contenttpl, atktext("explain_session_expired"), str_replace("'", "\\'", $destination), atktext("relogin"));
+    $box = $ui->renderBox(array("title" => $title, "content" => $content));
 
     $page->addContent($box);
     $output->output($page->render(atktext("title_session_expired"), true));
@@ -68,7 +69,7 @@
     if (!empty($lockType)) atklock();
 
     // Create node
-    $obj = &getNode($ATK_VARS["atknodetype"]);
+    $obj = &atkGetNode($ATK_VARS["atknodetype"]);
 
     $flags = array_key_exists("atkpartial", $ATK_VARS) ? HTML_PARTIAL : HTML_STRICT;
 
