9136a1ae0ea632a21b72d9c59ef803dbdbace102
guido (SVN Import) <info@achievo.org>
2007-05-02 14:16:05 +0000

    * Fixed atkBusy not being hided when an exception occurs. Now the exception   will be logged to the firebug debug console (if available). [Peter]

--- javascript/atkbusy.js
+++ javascript/atkbusy.js
@@ -1,13 +1,27 @@
 Ajax.Responders.register({
+  count: 0,
+
   onCreate: function() {
-    if ($('atkbusy') && Ajax.activeRequestCount > 0) {
+    this.count++;
+
+    if ($('atkbusy')) {
       $('atkbusy').style.visibility = 'visible';
     }
   },
-  
+
   onComplete: function() {
-    if ($('atkbusy') && Ajax.activeRequestCount == 0) {
+    this.count--;
+
+    if ($('atkbusy') && this.count == 0) {
       $('atkbusy').style.visibility = 'hidden';
+    }
+  },
+
+  onException: function(request, ex) {
+    this.onComplete();
+
+    if (typeof(console) == 'object' && console.debug) {
+      console.debug('Exception in Ajax.Request:', ex);
     }
   }
 });
