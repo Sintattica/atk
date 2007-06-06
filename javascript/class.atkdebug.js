7ea45105e548b30f6b060639527250afb1dd99cb
peter (SVN Import) <info@achievo.org>
2007-06-06 14:34:40 +0000

    + Added support for showing debug messages for Ajax requests. [Peter] * Fixed CSV row count in atkImportHandler. [Peter]

---
+++ javascript/class.atkdebug.js
@@ -0,0 +1,33 @@
+if (!window.ATK) {
+  var ATK = {};
+}
+
+ATK.Debug = {
+  ELEMENT_ID: 'atk_debugging_div',
+  
+  EXPANDED_CLASS: 'atkDebugExpanded',
+  COLLAPSED_CLASS: 'atkDebugCollapsed',
+  
+  /**
+   * Add content to the debugging div.
+   */
+  addContent: function(content) {
+    var el = document.getElementById(ATK.Debug.ELEMENT_ID);
+    if (el != null) {
+      el.innerHTML += content;
+    }
+  },
+  
+  /**
+   * Toggle an ATK debug block.
+   */
+  toggle: function(toggle) {
+    var block = toggle.parentNode;
+
+    if (block.className.match(ATK.Debug.COLLAPSED_CLASS)) {    
+      block.className = block.className.replace(ATK.Debug.COLLAPSED_CLASS, ATK.Debug.EXPANDED_CLASS);    
+    } else {
+      block.className = block.className.replace(ATK.Debug.EXPANDED_CLASS, ATK.Debug.COLLAPSED_CLASS);    
+    }
+  }
+};
