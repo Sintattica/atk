60dc23e6f69ca08c89c0f546f309b3ccecb1fbef
dennis (SVN Import) <info@achievo.org>
2007-07-23 13:59:13 +0000

    * After saving the data of a manytoonerelation using the editdialog   functionality, now only the cell that was changed will be refreshed.   This also fixes a bug where after saving a record with the editdialog   ATK would crash. [Dennis, Peter] * atkDateTimeAttribute does not have a AF_SEARCHABLE flag anymore at default.   [Peter, Dennis]

--- javascript/class.atkattribute.js
+++ javascript/class.atkattribute.js
@@ -5,7 +5,7 @@ if (!window.ATK) {
 ATK.Attribute = {
   /**
    * Refresh the attribute input form using Ajax.
-   */   
+   */
   refresh: function(url, focusFirstFormEl) {
     var elements = Form.getElements('entryform');
     var queryComponents = new Array();
@@ -17,26 +17,25 @@ ATK.Attribute = {
           queryComponents.push(queryComponent);
       }
     }
-    
+
     atkErrorFields.each(function(field) {
-      var queryComponent = $H({ 'atkerrorfields[]': field }).toQueryString();      
+      var queryComponent = $H({ 'atkerrorfields[]': field }).toQueryString();
       queryComponents.push(queryComponent);
     });
 
-    var params = queryComponents.join('&');  
-            
+    var params = queryComponents.join('&');
+
     var func = function(transport) { transport.responseText.evalScripts(); };
     if (focusFirstFormEl) {
       func = function(transport) { transport.responseText.evalScripts(); try { placeFocus(); } catch (ex) {} };
     }
 
-    
     /*
     // disable form fields that are going to be replaced
     $(field).getElementsBySelector('*').each(function(el) {
       el.style.visibility = 'hidden';
     });
-    
+
     var span = document.createElement('span');
     var img = document.createElement('img');
     img.src = 'themes/achievo_modern/images/spinner.gif';
@@ -45,7 +44,12 @@ ATK.Attribute = {
     Position.absolutize(span);
     Position.clone(field, span);
     */
-    
-    new Ajax.Request(url, { method: 'post', parameters: params, evalScripts: true, onComplete: func });        
+
+    new Ajax.Request(url, { method: 'post', parameters: params, evalScripts: true, onComplete: func });
+  },
+
+  refreshDisplay: function(url) {
+    var func = function(transport) { transport.responseText.evalScripts(); };
+    new Ajax.Request(url, { method: 'post', evalScripts: true, onComplete: func });
   }
 };
