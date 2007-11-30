93469a1bd7a7c3b21b39c806dc4946e442f81251
peter (SVN Import) <info@achievo.org>
2007-11-30 08:26:41 +0000

    * Getting closer, more to follow.

--- javascript/class.atkdatagrid.js
+++ javascript/class.atkdatagrid.js
@@ -8,8 +8,8 @@ ATK.DataGrid = {
   /**
    * Registers a data grid.
    */
-  register: function(name, formName, baseUrl) {
-    this.grids[name] = { name: name, formName: formName, baseUrl: baseUrl };
+  register: function(name, formName, baseUrl, embedded) {
+    this.grids[name] = { name: name, formName: formName, baseUrl: baseUrl, embedded: embedded };
   },
   
   /**
@@ -18,32 +18,61 @@ ATK.DataGrid = {
   get: function(name) {
     return this.grids[name];
   },
+  
+  /**
+   * Returns the grid form elements.
+   */
+  getElements: function(name) {
+   return $A($(name + '_container').getElementsByTagName('*')).inject([],
+      function(elements, child) {
+        if (Form.Element.Serializers[child.tagName.toLowerCase()])
+          elements.push(Element.extend(child));
+        return elements;
+      }
+    );  
+  },
 
   /**
    * Updates/refreshes the data grid with the given name. 
    *
-   * All current parameter values will be applied, except the ones override 
+   * All current parameter values will be applied, except the ones overriden 
    * by the overrides.
    */
-  update: function(name, overrides) {
+  update: function(name, plainOverrides, jsOverrides) {
+    var overrides = Object.extend(plainOverrides, jsOverrides);
     var grid = this.get(name);
-    console.debug(grid);
 
     var queryComponents = [];  
-    var elements = Form.getElements(grid.formName);
+    
+    // serialize the datagrid fields
+    var elements = this.getElements(name);
     elements.each(function(el) {
-      if (el.name && el.name.substring(0, name.length + 1) == name + '_') {
-        var key = el.name.substring(name.length + 1)
+      if (!el.disabled) {
+        var key = 'atkdg_AE_' + grid.name + '_AE_' + el.name;
         var value = $F(el);
         var queryComponent = encodeURIComponent(key) + '=' + encodeURIComponent(value);
         queryComponents.push(queryComponent);
       }
-    });
+    }); 
     
+    // overrides     
     $H(overrides).each(function(item) {
-      var queryComponent = encodeURIComponent(item.key) + '=' + encodeURIComponent(item.value);
+      var key = 'atkdg_AE_' + grid.name + '_AE_' + item.key;    
+      var queryComponent = encodeURIComponent(key) + '=' + encodeURIComponent(item.value);
       queryComponents.push(queryComponent);
-    });
+    });    
+    
+    // if embedded we also serialize the edit data form fields
+    if (grid.embedded) {
+      var elements = Form.getElements(grid.formName);
+      for (var i = 0; i < elements.length; i++) {
+        if (elements[i].name && elements[i].name.substring(0, 3) != 'atk') {
+          var queryComponent = Form.Element.serialize(elements[i]);
+          if (queryComponent)
+            queryComponents.push(queryComponent);
+        }
+      }
+    }      
 
     var params = queryComponents.join('&');
     var options = { parameters: params, evalScripts: true };
