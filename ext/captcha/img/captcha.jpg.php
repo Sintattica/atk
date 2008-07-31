cfae1e98596223b711cc041e02e0d50ff7402aa8
jvansluijs (SVN Import) <info@achievo.org>
2008-07-31 13:27:11 +0000

    Added Captcha-field to form-module

--- ext/captcha/img/captcha.jpg.php
+++ ext/captcha/img/captcha.jpg.php
@@ -6,7 +6,7 @@
    */
   
   // captcha directory
-  $captchaDir = "atk/ext/captcha/";
+  $captchaDir = atkconfig("atkroot")."atk/ext/captcha/";
 
   // include captcha class 
   require($captchaDir."php-captcha.inc.php");
