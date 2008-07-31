ff4bb077a8db3502ffaab4fab8d436eb456ec1ae
patrick (SVN Import) <info@achievo.org>
2008-07-31 09:13:00 +0000

    * In formfocus.js, the inital text selection is cleared to prevent data loss   on a key event. [Patrick]

--- javascript/formfocus.js
+++ javascript/formfocus.js
@@ -1,7 +1,7 @@
   /**
    * This file is part of the Achievo ATK distribution.
    * Detailed copyright and licensing information can be found
-   * in the doc/COPYRIGHT and doc/LICENSE files which should be 
+   * in the doc/COPYRIGHT and doc/LICENSE files which should be
    * included in the distribution.
    *
    * @package atk
@@ -13,43 +13,49 @@
    * @version $Revision$
    * $Id$
    */
-   
+
 function placeFocus(inEditForm)
 {
   if (typeof(inEditForm) == 'undefined')
     inEditForm = true;
-    
+
   if (document.forms.length == 0) return;
 
   var fields = document.forms[0].elements;
-  for (i = 0; i < fields.length; i++) 
-  { 
+  for (i = 0; i < fields.length; i++)
+  {
     var field = fields[i];
     if (!field.type) continue;
-    var type = field.type.toLowerCase();  
-     
-    if (type == "text" || type == "textarea" || type.toString().charAt(0) == "s") 
+    var type = field.type.toLowerCase();
+
+    if (type == "text" || type == "textarea" || type.toString().charAt(0) == "s")
     {
       if (!inEditForm)
       {
         field.focus();
+        var value = field.value;
+        field.value = '';
+        field.value = value;
         break;
       }
-      
+
       var found = false;
-      
+
       var node = field.parentNode;
       while (node != null)
       {
         if (node.nodeName.toLowerCase() == 'tr')
         {
-          
+
           found = node.id != null && node.id.substring(0, 3) == 'ar_' && node.style.display != 'none';
-          if (found) 
+          if (found)
           {
-            try 
+            try
             {
               field.focus();
+              var value = field.value;
+              field.value = '';
+              field.value = value;
             }
             catch (err)
             {
@@ -58,10 +64,10 @@ function placeFocus(inEditForm)
           }
           break;
         }
-        
+
         node = node.parentNode;
       }
-      
+
       if (found) break;
     }
   }
