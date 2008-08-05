10c56f68bec59da6d335d343ba5c4de371a77dc5
maurice (SVN Import) <info@achievo.org>
2008-08-05 19:59:45 +0000

    + Added atkcontroller var which can be used to load a custom controller. [Maurice] * Fixed navigation problems and error handling in AtkWizard for use within ATK application environment. [Maurice]

--- skel/dispatch.php
+++ skel/dispatch.php
@@ -68,13 +68,15 @@
     $lockType = atkconfig("lock_type");
     if (!empty($lockType)) atklock();
 
-    // Create node
-    $obj = &atkGetNode($ATK_VARS["atknodetype"]);
-
     $flags = array_key_exists("atkpartial", $ATK_VARS) ? HTML_PARTIAL : HTML_STRICT;
 
-    //Handle http request   
-    $controller = &atkinstance("atk.atkcontroller");
+    //Load controller   
+    if ($ATK_VARS["atkcontroller"]=="")
+      $controller = &atkinstance("atk.atkcontroller");
+    else
+      $controller = &atkinstance($ATK_VARS["atkcontroller"]);
+      
+    //Handle http request  
     $controller->dispatch($ATK_VARS, $flags);
   }
   $output->outputFlush();
