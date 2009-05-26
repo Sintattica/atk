f62292193e284470f93c10ae75bd311e83566546
lineke (SVN Import) <info@achievo.org>
2009-05-26 10:08:37 +0000

    + Added save export selection functionality.   Set $config_enable_export_save_selection to true to enable this functionality.   Also add the atk_exportcriteria table to your database, the mysql table   definition for this table can be found in skel/init_mysql.sql. [Ruud, Lineke]

--- skel/init_mysql.sql
+++ skel/init_mysql.sql
@@ -12,3 +12,15 @@ CREATE TABLE atk_searchcriteria
   handlertype VARCHAR(100) NOT NULL,
   PRIMARY KEY (nodetype, name)
 );
+
+CREATE TABLE atk_exportcriteria 
+(
+  id INT(11) NOT NULL,
+  nodetype VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  criteria TEXT NOT NULL,
+  user_id INT(11),
+  visibility TINYINT(1) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY( nodetype, name, user_id )
+);
