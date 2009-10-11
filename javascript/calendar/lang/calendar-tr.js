4baa4331ed763f8b2088514bf0f478e96a2fc71e
sandy (SVN Import) <info@achievo.org>
2009-10-11 09:03:25 +0000

    Updated languages

---
+++ javascript/calendar/lang/calendar-tr.js
@@ -0,0 +1,45 @@
+// ** I18N
+Calendar._DN = new Array
+("Pazar",
+ "Pazartesi",
+ "Salý",
+ "Çarþamba",
+ "Perþembe",
+ "Cuma",
+ "Cumartesi",
+ "Pazar");
+Calendar._MN = new Array
+("Ocak",
+ "Þubat",
+ "Mart",
+ "Nisan",
+ "Mayýs",
+ "Haziran",
+ "Temmuz",
+ "Aðustos",
+ "Eylül",
+ "Ekim",
+ "Kasým",
+ "Aralýk");
+
+// tooltips
+Calendar._TT = {};
+Calendar._TT["TOGGLE"] = "Haftanýn ilk gününü deðiþtir";
+Calendar._TT["PREV_YEAR"] = "Önceki yýl (menü için basýlý tutun)";
+Calendar._TT["PREV_MONTH"] = "Önceki ay (menü için basýlý tutun)";
+Calendar._TT["GO_TODAY"] = "Bugüne git";
+Calendar._TT["NEXT_MONTH"] = "Sonraki ay (menü için basýlý tutun)";
+Calendar._TT["NEXT_YEAR"] = "Sonraki yýl (menü için basýlý tutun)";
+Calendar._TT["SEL_DATE"] = "Tarih seçin";
+Calendar._TT["DRAG_TO_MOVE"] = "Taþýmak için sürükleyin";
+Calendar._TT["PART_TODAY"] = " (bugün)";
+Calendar._TT["MON_FIRST"] = "Ýlk gün Pazartesi";
+Calendar._TT["SUN_FIRST"] = "Ýlk gün Salý";
+Calendar._TT["CLOSE"] = "Kapat";
+Calendar._TT["TODAY"] = "Bugün";
+
+// date formats
+Calendar._TT["DEF_DATE_FORMAT"] = "dd-mm-y";
+Calendar._TT["TT_DATE_FORMAT"] = "d MM DD";
+
+Calendar._TT["WK"] = "Hf";
