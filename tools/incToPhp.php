355dfd745a8942c92113cf338ccaf12b3b7f72f4
Michele Rota <michele.rota@me.com>
2015-10-13 15:32:42 +0200

    tool to convert .inc to .php

---
+++ tools/incToPhp.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$path = realpath(__DIR__.'/../');
+
+$objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
+
+/**
+ * @var string $name
+ * @var SplFileInfo $object
+ */
+foreach($objects as $name => $object){
+    if($object->isFile() && $object->getExtension()==='inc') {
+        $src = $object->getRealPath();
+        $dst = substr($src, 0, -3).'php';
+        $command =  "git mv $src $dst";
+        echo $command."\n";
+        exec($command);
+    }
+}
