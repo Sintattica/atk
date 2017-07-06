92479ed9097e97caf2fd7a9ac56445be26c3c178
sanotto <sanotto@gmail.com>
2017-07-06 13:26:54 -0300

    Enabling default destination node/action
    The setDefaultDestination method of the IndexPage class was unused, after this change, adding a 'default_destination' => ['Module.Node', 'action'] entry  in config/atk.php will make the declared node and action the landing page for logged in users;

--- src/Core/Atk.php
+++ src/Core/Atk.php
@@ -126,6 +126,8 @@ class Atk
 
             /** @var IndexPage $indexPage */
             $indexPage = new $indexPageClass($this);
+            $default_destination = Config::getGlobal('default_destination');
+            $indexPage->setDefaultDestination($default_destination);
             $indexPage->generate();
         }
     }
