ce8a11bfba10deab2942f3a50841e9f9791e7124
Michele Rota <michele.rota@ymail.com>
2017-11-07 15:29:26 +0100

    Fix manytomanyselectrelation

--- src/Resources/public/javascript/class.atkmanytomanyselectrelation.js
+++ src/Resources/public/javascript/class.atkmanytomanyselectrelation.js
@@ -3,45 +3,33 @@ if (!window.ATK) {
 }
 
 ATK.ManyToManySelectRelation = {
-    /**
-     * Delete.
-     */
-    deleteItem: function(el) {
+    deleteItem: function (el) {
         var li = $(el).up('li');
         li.parentNode.removeChild(li);
     },
-    /**
-     * Add.
-     */
-    add: function(el, url, makeSortable) {
-        var params = {selector: $F(el)};
-        var li = $(el).up('li');
-        new Ajax.Request(
-            url, {
-                parameters: params,
-                onSuccess: function(response) {
-                    jQuery(li).find('select.ManyToOneRelation').empty();
-                    li.insert({before: response.responseText});
-                    response.responseText.evalScripts();
-                    if (makeSortable)
-                    {
-                        ATK.ManyToManySelectRelation.makeItemsSortable(li.up('ul'));
+    add: function (el, url, makeSortable) {
+        var selector = $F(el);
+        if (selector) {
+            new Ajax.Request(
+                url, {
+                    parameters: {selector: selector},
+                    onSuccess: function (response) {
+                        var li = $(el).up('li');
+                        var select = jQuery(li).find('select.ManyToOneRelation');
+                        select.val(null).trigger('change');
+                        li.insert({before: response.responseText});
+                        response.responseText.evalScripts();
+                        if (makeSortable) {
+                            ATK.ManyToManySelectRelation.makeItemsSortable(li.up('ul'));
+                        }
                     }
-                }
-            });
-
-        if (el.type == 'select-one')
-        {
-            el.selectedIndex = 0;
-        }
-        else
-        {
-            $(el.name + '_search').value = '';
-            el.value = '';
+                });
         }
     },
-    makeItemsSortable: function(id) {
-        Sortable.create($(id), {constraint: 'vertical',
-            only: 'atkmanytomanyselectrelation-selected'});
+    makeItemsSortable: function (id) {
+        Sortable.create($(id), {
+            constraint: 'vertical',
+            only: 'atkmanytomanyselectrelation-selected'
+        });
     }
-}
+};
