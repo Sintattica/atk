c46171b0d0e095ec1d814d11d93a5aca1ba3d729
Samuel BF <36460-samuelbf@users.noreply.framagit.org>
2019-04-08 09:48:47 +0200

    Removing atkfilter from Export (end of 8286b54b2a)

--- src/Handlers/ExportHandler.php
+++ src/Handlers/ExportHandler.php
@@ -577,11 +577,8 @@ class ExportHandler extends ActionHandler
             $node_bk->m_postvars['atksearchmode'] = $session_back['atkdg']['admin']['atksearchmode'];
         }
 
-        $atkfilter = Tools::atkArrayNvl($source, 'atkfilter', '');
-
-        $atkselector = isset($session_back['atkselector'])?$session_back['atkselector']:'';
-        $condition = $atkselector.($atkselector != '' && $atkfilter != '' ? ' AND ' : '').$atkfilter;
-        $recordset = $node_bk->select($condition)->orderBy($atkorderby)->includes($list_includes)->mode('export')->fetchAll();
+        $condition = isset($session_back['atkselector'])?$node_bk->primaryKeyFromString($session_back['atkselector']):'1';
+        $recordset = $node_bk->select($atkselector)->orderBy($atkorderby)->includes($list_includes)->mode('export')->fetchAll();
         if (method_exists($this->m_node, 'assignExportData')) {
             $this->m_node->assignExportData($list_includes, $recordset);
         }
