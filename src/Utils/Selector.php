Sintattica/atk

commit adbf9017aa92d03f8f6ee2abdd8deb511a03b959

Dropping SelectorIterator and query->build* functions
... in favor of execute* ones. Adding executeCount function, which returns an
integer.
SelectorIterator was only used in ManyToOneRelation could be replaced by a more
common fetchAll() call without significant performance harm (as I understand it).
Removing SelectorIterator allowed to remove the Query->buildSelect function
without changing the executeSelect return value.

src/Utils/Selector.php
@@ -16,7 +16,7 @@ use Exception;
  *
  * @author Peter C. Verhage <peter@achievo.org>
  */
-class Selector implements \ArrayAccess, \Countable, \IteratorAggregate
+class Selector implements \ArrayAccess, \Countable
 {
     /**
      * This selector's node.
@@ -61,27 +61,6 @@ class Selector implements \ArrayAccess, \Countable, \IteratorAggregate
      * @var array
      */
     protected $m_indices = null;
-
-    /**
-     * Current iterator instance (if iterator is used).
-     *
-     * @var SelectorIterator
-     */
-    private $m_iterator = null;
-
-    /**
-     * Current query object (if iterator is used).
-     *
-     * @var Query
-     */
-    private $m_query = null;
-
-    /**
-     * Current attributes by load type (if iterator is used).
-     *
-     * @var array
-     */
-    private $m_attrsByLoadType = null;
 
     /**
      * Constructor.
@@ -646,22 +625,6 @@ class Selector implements \ArrayAccess, \Countable, \IteratorAggregate
     }
 
     /**
-     * Transform raw database row to node compatible row for the current iterator.
-     *
-     * @param array $row raw database row
-     *
-     * @return array node compatible row
-     */
-    public function transformRow($row)
-    {
-        if ($this->m_iterator == null) {
-            throw new Exception(__METHOD__.' should only be called by the current atkSelectorIterator instance!');
-        }
-
-        return $this->_transformRow($row, $this->m_query, $this->m_attrsByLoadType);
-    }
-
-    /**
      * Returns the first found row.
      *
      * @return array first row
@@ -684,8 +647,7 @@ class Selector implements \ArrayAccess, \Countable, \IteratorAggregate
         if ($this->m_rows === null) {
             $attrsByLoadType = $this->_getAttributesByLoadType();
             $query = $this->_buildSelectQuery($attrsByLoadType);
-            $stmt = $this->_getDb()->queryP($query->buildSelect());
-            $rows = $stmt->fetchAll();
+            $rows = $query->executeSelect();
             $this->m_rows = $this->_transformRows($rows, $query, $attrsByLoadType);
         }
 
@@ -709,8 +671,7 @@ class Selector implements \ArrayAccess, \Countable, \IteratorAggregate
             $query->addExpression($field, 'SUM('.$expr.')', $prefix);
         }
 
-        $stmt = $this->_getDb()->queryP($query->buildSelect());
-        $row = $stmt->fetch();
+        $row = $query->executeSelect()[0];
 
         $query->deAlias($row);
         $res = [];
@@ -731,10 +692,7 @@ class Selector implements \ArrayAccess, \Countable, \IteratorAggregate
     {
         if ($this->m_rowCount === null) {
             $attrsByLoadType = $this->_getAttributesByLoadType();
-            $query = $this->_buildCountQuery($attrsByLoadType);
-            $stmt = $this->_getDb()->queryP($query->buildCount());
-            $rows = $stmt->fetchAll();
-            $this->m_rowCount = Tools::count($rows) == 1 ? $rows[0]['count'] : Tools::count($rows); // group by fix
+            $this->m_rowCount = $this->_buildCountQuery($attrsByLoadType)->executeCount();
         }
 
         return $this->m_rowCount;
@@ -771,8 +729,7 @@ class Selector implements \ArrayAccess, \Countable, \IteratorAggregate
         $query->addGroupBy($expression);
         $query->addOrderBy($expression);
 
-        $stmt = $this->_getDb()->queryP($query->buildSelect());
-        $this->m_indices = $stmt->getAllValues();
+        $this->m_indices = $query->executeSelect();
 
         return $this->m_indices;
     }
@@ -832,39 +789,10 @@ class Selector implements \ArrayAccess, \Countable, \IteratorAggregate
     }
 
     /**
-     * Returns this selector's iterator.
-     *
-     * NOTE: if you call this method multiple times, the same iterator will
-     *       be returned, unless you have closed the selector first
-     */
-    public function getIterator()
-    {
-        if ($this->m_iterator == null) {
-            $attrsByLoadType = $this->_getAttributesByLoadType();
-            $query = $this->_buildSelectQuery($attrsByLoadType);
-            $stmt = $this->_getDb()->queryP($query->buildSelect());
-
-            $this->m_attrsByLoadType = $attrsByLoadType;
-            $this->m_query = $query;
-
-            $this->m_iterator = new SelectorIterator($stmt, $this);
-        }
-
-        return $this->m_iterator;
-    }
-
-    /**
-     * Closes the current statement used for this selector.
-     * Also clears the row and row count cache.
+     * Clears the row and row count cache.
      */
     public function close()
     {
-        if ($this->m_iterator != null) {
-            $this->m_iterator = null;
-            $this->m_query = null;
-            $this->m_attrsByLoadType = null;
-        }
-
         $this->m_rows = null;
         $this->m_rowCount = null;
         $this->m_indices = null;

src/Db/Db.php
@@ -761,6 +761,8 @@ class Db extends \PDO
      * get NOW() or SYSDATE() equivalent for the current database.
      *
      * Every database has it's own implementation to get the current date
+     *
+     * @return string
      */
     public function func_now()
     {
@@ -770,9 +772,10 @@ class Db extends \PDO
     /**
      * get SUBSTRING() equivalent for the current database.
      *
-     * @param string $fieldname The database fieldname
+     * @param string $fieldname The database fieldname (already quoted)
      * @param int $startat The position to start from
      * @param int $length The number of characters
+     *
      * @return string
      */
     public function func_substring($fieldname, $startat = 0, $length = 0)
@@ -785,7 +788,7 @@ class Db extends \PDO
      * Each database driver should override this method to perform vendor
      * specific conversion.
      *
-     * @param string $fieldname The field to generate the to_char for.
+     * @param string $fieldname The field to generate the to_char for (already quoted)
      * @param string $format Format specifier. The format is compatible with
      *                          php's date() function (http://www.php.net/date)
      *                          The default is what's specified by
@@ -807,7 +810,7 @@ class Db extends \PDO
     /**
      * Get CONCAT_WS() equivalent for the current database.
      *
-     * @param array $fields
+     * @param array $fields (quoted)
      * @param string $separator
      * @param bool $remove_all_spaces remove all spaces in result (atkAggrecatedColumns searches for string without spaces)
      *
@@ -856,7 +859,7 @@ class Db extends \PDO
      *
      * TODO/FIXME: add format parameter. Current format is always yyyy-mm-dd hh:mi.
      *
-     * @param string $fieldname The field to generate the to_char for.
+     * @param string $fieldname The field to generate the to_char for (quoted).
      *
      * @return string Piece of sql query that converts a datetime field to char
      *                for the current database

src/Db/Query.php
@@ -541,15 +541,47 @@ class Query
     }
 
     /******************************** Builder/executer functions *****************************/
+    /**
+     * Builds and execute the SQL Select COUNT(*) query.
+     *
+     * This is different from select,
+     * because we do joins, like in a select, but we don't really select the
+     * fields.
+     *
+     * @return int $count
+     */
+    public function executeCount($distinct = false)
+    {
+        $query = new QueryPart('');
+        if (($distinct || $this->m_distinct) && !empty($this->m_fields)) {
+            $query->appendSql('SELECT COUNT(DISTINCT ');
+            $query->appendSql(implode(', ', $this->m_fields));
+            $query->appendSql(') as count FROM');
+        } else {
+            $query->appendSql('SELECT COUNT(*) AS count FROM');
+        }
+
+        $query->appendSql(Db::quoteIdentifier($this->m_table));
+        $query->appendSql(implode(' ', $this->m_joins));
+
+        $query->append($this->whereClause());
+
+        if (!empty($this->m_groupbys)) {
+           $query->appendSql(' GROUP BY '.implode(', ', $this->m_groupbys));
+        }
+
+        return $this->m_db->getValue($query);
+    }
 
     /**
-     * Builds the SQL Select query.
+     * Wrapper function to execute a select query.
      *
-     * @param bool $distinct distinct records?
+     * @param bool $distinct Set to true to perform a distinct select,
+     *                       false for a regular select.
      *
-     * @return QueryPart a SQL Select Query
+     * @return records[] array
      */
-    public function buildSelect($distinct = false)
+    public function executeSelect($distinct = false)
     {
         if (empty($this->m_fields) && empty($this->m_expressions)) {
             return false;
@@ -591,60 +623,15 @@ class Query
             $query->append($this->limiterClause());
         }
 
-        return $query;
+        return $this->getDb()->queryP($query)->fetchAll();
     }
 
     /**
-     * Builds the SQL Select COUNT(*) query. This is different from select,
-     * because we do joins, like in a select, but we don't really select the
-     * fields.
+     * Builds and execute the SQL Update query.
      *
-     * @return string a SQL Select COUNT(*) Query
+     * @return bool $success
      */
-    public function buildCount($distinct = false)
-    {
-        $query = new QueryPart('');
-        if (($distinct || $this->m_distinct) && !empty($this->m_fields)) {
-            $query->appendSql('SELECT COUNT(DISTINCT ');
-            $query->appendSql(implode(', ', $this->m_fields));
-            $query->appendSql(') as count FROM');
-        } else {
-            $query->appendSql('SELECT COUNT(*) AS count FROM');
-        }
-        
-        $query->appendSql(Db::quoteIdentifier($this->m_table));
-        $query->appendSql(implode(' ', $this->m_joins));
-
-        $query->append($this->whereClause());
-
-        if (!empty($this->m_groupbys)) {
-           $query->appendSql(' GROUP BY '.implode(', ', $this->m_groupbys));
-        }
-
-        return $query;
-    }
-
-    /**
-     * Wrapper function to execute a select query.
-     *
-     * @param bool $distinct Set to true to perform a distinct select,
-     *                       false for a regular select.
-     *
-     * @return array The set of records returned by the database.
-     */
-    public function executeSelect($distinct = false)
-    {
-        $query = $this->buildSelect($distinct);
-
-        return $this->getDb()->getRows($query);
-    }
-
-    /**
-     * Builds the SQL Update query.
-     *
-     * @return string a SQL Update Query
-     */
-    public function buildUpdate()
+    public function executeUpdate($distinct = false)
     {
         $query = new QueryPart('UPDATE '.Db::quoteIdentifier($this->m_table).' SET');
 
@@ -657,25 +644,15 @@ class Query
 
         $query->append($this->whereClause());
 
-        return $query;
+        return ($this->getDb()->queryP($query) != false);
     }
 
     /**
-     * Wrapper function to execute an update query.
-     */
-    public function executeUpdate()
-    {
-        $query = $this->buildUpdate();
-
-        return $this->getDb()->queryP($query);
-    }
-
-    /**
-     * Builds the SQL Insert query.
+     * Builds & execute the SQL Insert query.
      *
-     * @return string a SQL Insert Query
+     * @return bool $success
      */
-    public function buildInsert()
+    public function executeInsert($distinct = false)
     {
         $query = new QueryPart('INSERT INTO '.Db::quoteIdentifier($this->m_table).'');
 
@@ -687,45 +664,26 @@ class Query
         $query->append(new QueryPart(implode(', ', array_keys($parameters)), $parameters));
         $query->appendSql(')');
 
-        return $query;
-    }
-
-    /**
-     * Wrapper function to execute an insert query.
-     */
-    public function executeInsert()
-    {
-        $query = $this->buildInsert();
-
         $result = $this->getDb()->queryP($query);
         if ($result && $this->m_returnSeqValue) {
             $this->m_seqValue = $this->getDb()->lastInsertId($this->m_seqName);
             Tools::atkdebug("Value for sequence {$this->m_seqName}: {$this->m_seqValue}");
         }
 
-        return $result;
+        return ($result != false);
     }
 
     /**
-     * Builds the SQL Delete query.
+     * Execute a SQL delete query
      *
-     * @return string a SQL Delete Query
+     * @return bool $success
      */
-    public function buildDelete()
+    public function executeDelete($distinct = false)
     {
         $query = new QueryPart('DELETE FROM '.Db::quoteIdentifier($this->m_table));
         $query->append($this->whereClause());
-        return $query;
-    }
 
-    /**
-     * Wrapper function to execute a delete query.
-     */
-    public function executeDelete()
-    {
-        $query = $this->buildDelete();
-
-        return $this->getDb()->queryP($query);
+        return ($this->getDb()->queryP($query) != false);
     }
 
     /**************************** Search functions **********************************************

src/Relations/ManyToOneRelation.php
@@ -2177,7 +2177,7 @@ EOF;
         $selector = $this->_getSelectableRecordsSelector($record, $mode);
         $selector->limit($limit, $offset);
         $count = $selector->getRowCount();
-        $iterator = $selector->getIterator();
+        $rows = $selector->fetchAll();
         $more = ($offset + $limit > $count) ? 'false' : 'true';
 
         $result .= '<div id="total">'.$count.'</div>'."\n";
@@ -2185,7 +2185,7 @@ EOF;
         $result .= '<div id="more">'.$more.'</div>'."\n";
 
         $result .= '<ul>';
-        foreach ($iterator as $rec) {
+        foreach ($rows as $rec) {
             $option = $this->m_destInstance->descriptor($rec);
             $value = $this->m_destInstance->primaryKey($rec);
             $result .= '
@@ -2221,7 +2221,7 @@ EOF;
         $selector = $this->_getSelectableRecordsSelector($record, $mode);
         $selector->limit($limit, $offset);
         $count = $selector->getRowCount();
-        $iterator = $selector->getIterator();
+        $rows = $selector->fetchAll();
         $more = ($offset + $limit > $count) ? 'false' : 'true';
 
         $result .= '<div id="total">'.$count.'</div>'."\n";
@@ -2229,7 +2229,7 @@ EOF;
         $result .= '<div id="more">'.$more.'</div>'."\n";
 
         $result .= '<ul>';
-        foreach ($iterator as $rec) {
+        foreach ($rows as $rec) {
             $option = $value = $this->m_destInstance->descriptor($rec);
             $result .= '
           <li value="'.htmlentities($value).'">'.htmlentities($option).'</li>';

src/Security/Auth/DbAuth.php
@@ -180,34 +180,27 @@ class DbAuth extends AuthInterface
         $accountenableexpression = Config::getGlobal('auth_accountenableexpression');
 
         $db = Db::getInstance(Config::getGlobal('auth_database'));
-        if ($usertable == $leveltable || $leveltable == '') {
-            // Level and userid are stored in the same table.
-            // This means one user can only have one level.
-            $query = "SELECT * FROM $usertable WHERE $userfield ='$user'";
-        } else {
+        $query = $db->createQuery();
+        $query->addTable($usertable)
+            ->addAllFields()
+            ->addCondition(Db::quoteIdentifier($usertable).'.'.Db::quoteIdentifier($userfield).'=:user', [':user' => [$user]]);
+        if ($usertable != $leveltable && $leveltable != '') {
             // Level and userid are stored in two separate tables. This could
             // mean (but doesn't have to) that a user can have more than one
             // level.
-            $qryobj = $db->createQuery();
-            $qryobj->setTable($usertable);
-            $qryobj->addAllFields($usertable);
-            $qryobj->addField($levelfield, '', 'usergroup');
-            $qryobj->addJoin($leveltable, 'usergroup', "$usertable.$userpk = usergroup.$userfk", true);
-            $qryobj->addCondition("$usertable.$userfield = '$user'");
+            $query->addJoin($leveltable, 'usergroup', "$usertable.$userpk = usergroup.$userfk", true);
+            $query->addField($levelfield, '', 'usergroup');
 
             if (!empty($groupparentfield)) {
-                $qryobj->addField("grp.$groupparentfield");
-                $qryobj->addJoin($grouptable, 'grp', "usergroup.$levelfield = grp.$groupfield", true);
+                $query->addJoin($grouptable, 'grp', "usergroup.$levelfield = grp.$groupfield", true);
+                $query->addField("grp.$groupparentfield");
             }
-            $query = $qryobj->buildSelect();
         }
 
         if ($accountenableexpression) {
-            $query .= " AND $accountenableexpression";
+            $query->addCondition(" AND $accountenableexpression");
         }
-        $recs = $db->getRows($query);
-
-        return $recs;
+        return $query->executeSelect();
     }
 
     /**
@@ -229,9 +222,8 @@ class DbAuth extends AuthInterface
         $query->addField($groupparentfield);
         $query->setTable($grouptable);
         $query->addCondition("$grouptable.$groupfield IN (".implode(',', $parents).')');
-        $recs = $db->getRows($query->buildSelect(true));
 
-        return $recs;
+        return $query->executeSelect(true);
     }
 
     /**

src/Security/Auth/SspiAuth.php
@@ -129,30 +129,23 @@ class SspiAuth extends DbAuth
         $groupparentfield = Config::getGlobal('auth_groupparentfield');
 
         $db = Db::getInstance(Config::getGlobal('auth_database'));
-        if ($usertable == $leveltable || $leveltable == '') {
-            // Level and userid are stored in the same table.
-            // This means one user can only have one level.
-            $query = "SELECT * FROM $usertable WHERE $sspifield ='$user'";
-        } else {
+        $query = $db->createQuery();
+        $query->setTable($userTable)
+            ->addAllFields($usertable)
+            ->addCondition(Db::quoteIdentifier($usertable).'.'.Db::quoteIdentifier($sspifield).'=:user', [':user' => [$user]]);
+        if ($usertable != $leveltable && $leveltable != '') {
             // Level and userid are stored in two separate tables. This could
             // mean (but doesn't have to) that a user can have more than one
             // level.
-            $qryobj = $db->createQuery();
-            $qryobj->setTable($usertable);
-            $qryobj->addAllFields($usertable);
-            $qryobj->addAllFields('usergroup');
-            $qryobj->addJoin($leveltable, 'usergroup', "$usertable.$userpk = usergroup.$userfk", true);
-            $qryobj->addCondition("$usertable.$sspifield = '$user'");
+            $query->addJoin($leveltable, 'usergroup', "$usertable.$userpk = usergroup.$userfk", true);
+            $query->addAllFields('usergroup');
 
             if (!empty($groupparentfield)) {
-                $qryobj->addField($groupparentfield, '', 'grp');
-                $qryobj->addJoin($grouptable, 'grp', "usergroup.$levelfield = grp.$groupfield", true);
+                $query->addJoin($grouptable, 'grp', "usergroup.$levelfield = grp.$groupfield", true);
+                $query->addField($groupparentfield, '', 'grp');
             }
-            $query = $qryobj->buildSelect();
         }
-        $recs = $db->getRows($query);
-
-        return $recs;
+        return $query->executeSelect();
     }
 
     public function getUser(&$user)

src/Utils/SelectorIterator.php
@@ -1,58 +1,0 @@
-<?php
-
-namespace Sintattica\Atk\Utils;
-
-use IteratorIterator;
-use Iterator;
-
-/**
- * Selector iterator, makes sure that each each row returned by the internal
- * iterator gets transformed before it is returned to the user.
- *
- * @author Peter C. Verhage <peter@achievo.org>
- */
-class SelectorIterator extends IteratorIterator
-{
-    /**
-     * Selector.
-     *
-     * @var Selector
-     */
-    private $m_selector;
-
-    /**
-     * Constructor.
-     *
-     * @param Iterator $iterator iterator
-     * @param Selector $selector selector
-     */
-    public function __construct(\Traversable $iterator, Selector $selector)
-    {
-        parent::__construct($iterator);
-        $this->m_selector = $selector;
-    }
-
-    /**
-     * Returns the selector.
-     *
-     * @return Selector selector
-     */
-    public function getSelector()
-    {
-        return $this->m_selector;
-    }
-
-    /**
-     * Returns the current row transformed.
-     */
-    public function current()
-    {
-        $row = parent::current();
-
-        if ($row != null) {
-            $row = $this->getSelector()->transformRow($row);
-        }
-
-        return $row;
-    }
-}

commit 55e3b154afbc522d07cb44acc86bf6deb108a73b

Fix index on datagrid
This bug was probably introduced by adbf9017a.

src/Utils/Selector.php
@@ -660,7 +660,10 @@ class Selector implements \ArrayAccess, \Countable
         $query->addGroupBy($expression);
         $query->addOrderBy($expression);
 
-        $this->m_indices = $query->executeSelect();
+        $this->m_indices = [];
+        foreach ($query->executeSelect() as $row) {
+            $this->m_indices[] = $row['index'];
+        }
 
         return $this->m_indices;
     }