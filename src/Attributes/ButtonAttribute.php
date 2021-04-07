280c620e4bab7e4e60bfa60a3505f162567c8901
Emanuele Trivella <emanuele.trivella@sintattica.it>
2021-04-07 15:34:04 +0200

    update ButtonAttribute

--- src/Attributes/ButtonAttribute.php
+++ src/Attributes/ButtonAttribute.php
@@ -3,59 +3,71 @@
 namespace Sintattica\Atk\Attributes;
 
 use Sintattica\Atk\Core\Atk;
+use Sintattica\Atk\Core\Node;
 use Sintattica\Atk\Core\Tools;
 use Sintattica\Atk\Session\SessionManager;
 
 class ButtonAttribute extends DummyAttribute
 {
-    private $nodeUri;
-    private $action;
-    private $params;
-    private $name;
-    private $class;
-    private $target;
-    private $sessionStatus;
-    private $saveForm;
+    const TYPE_SUBMIT = 'submit'; // default
+    const TYPE_ACTION = 'action';
 
-    public function __construct($name, $flags = 0, $owner, $action, $options = [])
+    private $name;
+    private $classes = ['btn', 'btn-default'];
+    private $node = '';
+    private $action = '';
+    private $params = [];
+    private $target = null;
+    private $sessionStatus = SessionManager::SESSION_NESTED;
+    private $saveForm = false;
+    private $type = self::TYPE_SUBMIT;
+    private $callback = null;
+
+    public function __construct($name, $flags = 0)
     {
         $this->name = $name;
-        $this->nodeUri = $owner;
-        $this->action = $action;
-        // TODO: remove?
-        $this->params = isset($options['params']) ? $options['params'] : [];
-        $this->class = 'btn btn-default' . (isset($options['class']) ? ' ' . $options['class'] : '');
-        $this->target = isset($options['target']) ? $options['target'] : null;
-        $this->sessionStatus = isset($options['sessionStatus']) ? $options['sessionStatus'] : SessionManager::SESSION_NESTED;
-        $this->saveForm = isset($options['saveForm']) ? $options['saveForm'] : false;
 
         parent::__construct($name, $flags | self::AF_HIDE_LIST | self::AF_READONLY);
     }
 
     public function display($record, $mode)
     {
-        $ownerInstance = Atk::getInstance()->atkGetNode($this->nodeUri);
-        $action = $this->action;
-        if ($action == 'edit') {
-            // check if the user has the permission, otherwise change in view action
-            $action = $ownerInstance->allowed('edit') ? 'edit' : 'view';
+        $classes = implode(' ', $this->classes);
+
+        switch ($this->type) {
+            case self::TYPE_ACTION:
+                /** @var Node $ownerInstance */
+                $ownerInstance = Atk::getInstance()->atkGetNode($this->node);
+                $action = $this->action;
+                if ($action === 'edit' and !$ownerInstance->allowed('edit')) {
+                    // action edit ma l'utente non ha i permessi: mando in view
+                    $action = 'view';
+                }
+                $this->params['atkselector'] = $ownerInstance->getPrimaryKey($record);
+                $url = Tools::dispatch_url($this->node, $action, $this->params);
+                $extraProps = [];
+                if ($classes) {
+                    $extraProps[] = 'class="' . $classes . '"';
+                }
+                if ($this->target) {
+                    $extraProps[] = 'target="' . $this->target . '"';
+                }
+                return Tools::href($url, Tools::atktext($this->name, $ownerInstance->getModule()), $this->sessionStatus, $this->saveForm, implode(' ', $extraProps));
+
+            case self::TYPE_SUBMIT:
+            default:
+                $txt = $this->text($this->name);
+                return '<button type="submit" class="' . $classes . '" name="' . $this->name . '" value="' . $txt . '">' . $txt . '</button>';
         }
-        $this->params['atkselector'] = $ownerInstance->getPrimaryKey($record);
-        $url = Tools::dispatch_url($this->nodeUri, $action, $this->params);
-        $extraProps = 'class="' . $this->class . '"';
-        if ($this->target) {
-            $extraProps .= ' target="' . $this->target . '"';
-        }
-        return Tools::href($url, Tools::atktext($this->name, $ownerInstance->getModule()), $this->sessionStatus, $this->saveForm, $extraProps);
     }
 
-    public function addParam(string $key, string $value): self
+    public function addParam($key, $value): self
     {
         $this->params[$key] = $value;
         return $this;
     }
 
-    public function addParams(array $params = []): self
+    public function addParams($params = []): self
     {
         $this->params = array_merge($this->params, $params);
         return $this;
@@ -67,16 +79,75 @@ class ButtonAttribute extends DummyAttribute
         return $this;
     }
 
-    public function setClass(string $class): self
+    public function getParams(): array
     {
-        $this->class = $class;
+        return $this->params;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name): self
+    {
+        $this->name = $name;
         return $this;
     }
 
-    public function setTarget(string $target): self
+    public function getNode(): string
+    {
+        return $this->node;
+    }
+
+    public function setNode(string $node): self
+    {
+        $this->node = $node;
+        return $this;
+    }
+
+    public function getAction(): string
+    {
+        return $this->action;
+    }
+
+    public function setAction($action): self
+    {
+        $this->action = $action;
+        return $this;
+    }
+
+    public function getClasses(): array
+    {
+        return $this->classes;
+    }
+
+    public function setClasses(array $cssClasses): self
+    {
+        $this->classes = $cssClasses;
+        return $this;
+    }
+
+    public function addClass(string $class): self
+    {
+        $this->classes[] = $class;
+        return $this;
+    }
+
+    public function getTarget(): string
+    {
+        return $this->target;
+    }
+
+    public function setTarget($target): self
     {
         $this->target = $target;
         return $this;
+    }
+
+    public function getSessionStatus(): int
+    {
+        return $this->sessionStatus;
     }
 
     public function setSessionStatus(int $sessionStatus): self
@@ -85,10 +156,36 @@ class ButtonAttribute extends DummyAttribute
         return $this;
     }
 
+    public function getSaveForm(): bool
+    {
+        return $this->saveForm;
+    }
+
     public function setSaveForm(bool $saveForm): self
     {
         $this->saveForm = $saveForm;
         return $this;
     }
 
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getCallback(): callable
+    {
+        return $this->callback;
+    }
+
+    public function setCallback(callable $callback): self
+    {
+        $this->callback = $callback;
+        return $this;
+    }
 }
