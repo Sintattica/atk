68c874d977040ac312ecdf2946533967e7f198b2
Matteo Castellini <matteo.castellini@sintattica.it>
2022-10-07 17:19:29 +0200

    fix(atk): fix search in NestedDateTimeAttribute

--- src/Attributes/NestedAttributes/NestedDateTimeAttribute.php
+++ src/Attributes/NestedAttributes/NestedDateTimeAttribute.php
@@ -1,11 +1,8 @@
 <?php
 
-
 namespace Sintattica\Atk\Attributes\NestedAttributes;
 
-
 use Sintattica\Atk\Attributes\DateTimeAttribute;
-use Sintattica\Atk\Db\Query;
 
 class NestedDateTimeAttribute extends DateTimeAttribute
 {
@@ -25,15 +22,15 @@ class NestedDateTimeAttribute extends DateTimeAttribute
         return parent::getOrderByStatement($extra, $table, $direction);
     }
 
-    public function getSearchCondition(Query $query, $table, $value, $searchmode, $fieldname = '')
+    public function searchCondition($query, $table, $value, $searchmode, $fieldaliasprefix = '')
     {
         if (!$this->getOwnerInstance()->isNestedAttribute($this->fieldName())) {
-            return parent::getSearchCondition($query, $table, $value, $searchmode, $fieldname);
+            parent::searchCondition($query, $table, $value, $searchmode, $fieldaliasprefix);
         }
-        return parent::getSearchCondition($query, $table, $value, $searchmode, $this->buildSQLSearchValue($table));
+        $this->m_date->searchCondition($query, $table, $value, $searchmode, $this->buildSQLSearchValue($table));
     }
 
-    protected function buildSQLSearchValue($table)
+    protected function buildSQLSearchValue($table): string
     {
         return "JSON_UNQUOTE(" . NestedAttribute::buildJSONExtractValue($this, $table) . ")";
     }
